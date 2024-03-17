af44381feeb5a0f5c93dd1658874807d0d1e5308
German-Karpa <german.karpa@allievi.bearzi.it>
2024-03-17 13:56:50 +0100

    fetch degli ordini su js

--- ordini.php
+++ ordini.php
@@ -1,11 +1,10 @@
-<?php include("./API/ordini/riepilogo.php") ?>
-
 <!DOCTYPE html>
 <html lang="it">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="ordersStyle.css">
+    <script src='g.js'></script>
     <title>orders</title>
   </head>
   <body>
@@ -13,14 +12,8 @@
 
     <!-- Per user -->
     <?php if($ruolo !== 'amministratore'): ?>
-      
-      <form action="riepilogoOrdini.php" method="GET">
-        <input class="searchInput" type="text" name="keyword" id="keyword" placeholder="Inserisci nome prodotto">
-        <button class="search" type="submit">Cerca</button>
-      </form>
-
       <?php foreach ($ordini as $ordine): ?>
-        <div>
+        <div id='user_view'>
           <p>Nome: <?php echo $ordine['nome_prodotto']; ?></p>
           <p>Quantità: <?php echo $ordine['quantita_totale']; ?></p>
           <p>Prezzo: &euro; <?php echo number_format($ordine['totale_ordine'], 2); ?></p>
@@ -30,7 +23,6 @@
 
     <!-- Per admin -->
     <?php if($ruolo == 'amministratore'): ?>
-
       <form action="riepilogoOrdini.php" method="GET">
         <select class="searchInput" name="action" id="action">
           <option value="numero_ordine">Numero ordine</option>
@@ -42,7 +34,7 @@
       </form>
 
       <?php foreach ($ordini as $ordine): ?>
-        <div>
+        <div id='admin_view'>
           <p>Cliente: <?php echo $ordine['nome_cliente']." ".$ordine['cognome_cliente']; ?></p>
           <p id="<?php echo $ordine['id_ordine']; ?>"><?php echo $ordine['numero_ordine']; ?></p>
           <p>Data: <?php echo $ordine['data_ordine']; ?></p>
